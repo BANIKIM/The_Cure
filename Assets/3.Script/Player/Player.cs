4f092e01d42fce2e1545f5720a07b92c1f4595e3
BANIKIM <72877425+BANIKIM@users.noreply.github.com>
2023-10-03 01:47:16 +0900

    202031003
    무한맵구현
    캐릭터 애니메이션 제작

--- Assets/3.Script/Player/Player.cs
+++ Assets/3.Script/Player/Player.cs
@@ -7,11 +7,16 @@ public class Player : MonoBehaviour
     [SerializeField] private float MoveSpeed=0;
     public Vector2 inputVec;
 
-    Rigidbody2D rigid;
+    Rigidbody2D rigid;//캐릭터 움직임을 위해서 선언
+    SpriteRenderer spriter; //좌우 변경모션을 만들기 위해서 선언
+    Animator anim; //애니메에션 값을 변경을 하기 위해
+    
 
-    private void Awake()
+     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
 
@@ -23,7 +28,7 @@ public class Player : MonoBehaviour
         inputVec.y = Input.GetAxisRaw("Vertical"); // 상하
     }
 
-    private void FixedUpdate()
+     void FixedUpdate()
     {
         //힘을 준다. 얼음판위에 돌을 밀듯이 밀린다.
         //rigid.AddForce(inputVec);
@@ -32,5 +37,18 @@ public class Player : MonoBehaviour
         //위치 이동  - 수업시간에서 많이 사용한 위치 이동 속도가 너무 빠르고..
         Vector2 Moveto = inputVec.normalized * MoveSpeed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position+ Moveto);
+    }
+
+
+     void LateUpdate()
+    {
+        
+        anim.SetFloat("Speed", inputVec.magnitude); //magnitude 순수하게 크기만 가지고 있는 값
+        //SetFloat Float의 값을 변경할 수 있음 앞의 ""은 애니메이터에 선언된 이름을 적고 , 뒤의 값은 넣을값을 대입하면됨
+
+        if (inputVec.x != 0)//이동키가 입력이 되면
+        {
+            spriter.flipX = inputVec.x < 0; //비교연산자의 결과를 바로 넣을 수 있음 크면 true가 되고 작으면 false가 된다.
+        }
     }
 }
---
+++ Assets/3.Script/Player/Reposition.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reposition : MonoBehaviour
+{
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Area"))
+            return;
+        
+
+        Vector3 playerPos = GameManager.instance.player.transform.position; //플레이어 위치저장
+        Vector3 myPos = transform.position;//내 포지션 저장
+
+        float diffx =Mathf.Abs(playerPos.x - myPos.x);//Mathf.Abs 음수값이 아닌 양수값만 받음 
+        float diffy = Mathf.Abs(playerPos.y - myPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+
+        switch (transform.tag)
+        {
+            case "Ground":
+                if(diffx > diffy)
+                {
+                    transform.Translate(Vector3.right* dirX * 40);
+                }
+                else if (diffx < diffy)
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+
+                break;
+            case "Enemy":
+
+                break;
+        }
+
+    }
+}
